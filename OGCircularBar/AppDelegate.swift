967ad6634590291d5c64b8d60de1a2fc81e46859
Oskar Groth <oskar@cindori.org>
2017-04-12 16:01:54 +0200

    Initial Commit

---
+++ OGCircularBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OGCircularBar
+//
+//  Created by Oskar Groth on 2017-04-12.
+//  Copyright © 2017 Oskar Groth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
